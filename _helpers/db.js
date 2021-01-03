3c5ddb16c914356a7469a41c8223e5f708c81e2e
Yassine <44257091+bbluxe@users.noreply.github.com>
2021-01-03 15:15:09 +0100

    Update

--- _helpers/db.js
+++ _helpers/db.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(env.database, env.username, env.password, {
   host: env.host,
-  dialect: "postgres"
+  dialect: env.dialect
 });
 
 const test = new Sequelize
