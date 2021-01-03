483f777fdd66db7b719f1d34d03839705a2cf3d9
Eric Chheu <eric.chheu@epitech.eu>
2021-01-03 16:18:42 +0100

    Change port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
